5db09ec33e54b56ac405ee0b137a69826da3e5bf
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-02-06 11:36:15 -0800

    Update (odd and random)
    This update includes a random list generator and a odd number detector for the random list.

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import random
+
+# Base Challenge with given list
+
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 b = [num for num in a if num % 2 == 0]
 
+print(a)
 print(b)
+
+# Self-induced dilema: create a random list assigned to 
+# a variable and get only the odd numbered list items
+
+list_length = random.randint(5, 20)
+
+c = []
+
+count = 0
+
+while count <= list_length:
+    c.append(random.randint(0, 200))
+    count += 1
+
+d = [num for num in c if num % 2 == 1]
+
+print(c)
+print(d)
+
+
+
